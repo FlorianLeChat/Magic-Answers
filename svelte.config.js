1a98cdd5c78da039ff7acc9f9d5a4de670388267
Florian Trayon <git@florian-trayon.fr>
2026-04-24 11:01:42 +0200

    fix(eslint): include config files to prevent parsing errors

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,13 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
+        typescript: {
+            config: ( config ) =>
+            {
+                config.include.push( "../*.config.*" );
+                return config;
+            }
+        },
         adapter: adapter(),
         paths: {
             // @ts-expect-error Needed for GitLab Pages generation.
