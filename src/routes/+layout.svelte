abf0d5c1be627009e119bcbf1c8ba6e93a618208
Florian Trayon <git@florian-trayon.fr>
2026-08-15 19:35:59 +0200

    feat(analytics): replace Google Analytics with Umami analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import "../app.css";
+    import { env } from "$env/dynamic/public";
     import { asset } from "$app/paths";
 
     let { children } = $props();
@@ -23,6 +24,18 @@
     />
 
     <link rel="manifest" href={asset( "/manifest.webmanifest" )} crossorigin="use-credentials" />
+
+    {#if env.PUBLIC_ANALYTICS_ENABLED === "true"}
+        <script
+            src={env.PUBLIC_ANALYTICS_ENDPOINT}
+            defer
+            data-website-id={env.PUBLIC_ANALYTICS_PROJECT_ID}
+            data-performance="true"
+            data-do-not-track={env.PUBLIC_ANALYTICS_RESPECT_DNT}
+            data-exclude-hash="true"
+            data-exclude-search="true"
+        ></script>
+    {/if}
 </svelte:head>
 
 {@render children()}
